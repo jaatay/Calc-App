c0293ded1fda62b1832abfc2c06e6979c081d095
Jesse Atay <jesse.atay@gmail.com>
2018-06-03 18:16:16 -0700

    Add project files.

---
+++ Calc App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calc_App
+{
+    class Program
+    {
+
+        double selectedNumber;
+
+        static void Main(string[] args)
+        {
+            FirstScreen();
+            
+        }
+        //methods
+        public FirstScreen(number){
+            Console.WriteLine("Select a number");
+           double number = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(selectedNumber);
+            return number;
+            }
+        
+}
+}
